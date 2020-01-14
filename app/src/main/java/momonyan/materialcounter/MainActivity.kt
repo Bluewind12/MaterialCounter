62efa4ab1f27f0d68e8667d5e0ca0a7696f85caa
“BlueWind_Mac” <zero18030@gmail.com>
2020-01-14 18:45:11 +0900

    [update]ViewModelでDB呼び出し

--- app/src/main/java/momonyan/materialcounter/MainActivity.kt
+++ app/src/main/java/momonyan/materialcounter/MainActivity.kt
@@ -4,14 +4,21 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
+import androidx.room.Room
 import momonyan.materialcounter.databinding.ActivityMainBinding
+import momonyan.materialcounter.repository.MaterialDataBase
 import momonyan.materialcounter.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
+    var instance: MaterialDataBase? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        instance =
+            Room.databaseBuilder(applicationContext, MaterialDataBase::class.java, "Material")
+                .build()
 
         // ViewModelのインスタンスを作成
         val viewModel: MainViewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
--- app/src/main/java/momonyan/materialcounter/viewmodel/MainViewModel.kt
+++ app/src/main/java/momonyan/materialcounter/viewmodel/MainViewModel.kt
@@ -1,11 +1,17 @@
 package momonyan.materialcounter.viewmodel
 
+import android.app.Application
 import android.util.Log
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.AndroidViewModel
+import androidx.room.Room
+import momonyan.materialcounter.repository.MaterialDataBase
 
-class MainViewModel :ViewModel(){
+class MainViewModel(application: Application) : AndroidViewModel(application) {
 
-    fun addData(){
-        Log.d("TEST","FABClick")
+    val database =
+        Room.databaseBuilder(application, MaterialDataBase::class.java, "Materials").build()
+
+    fun addData() {
+        Log.d("TEST", "FABClick")
     }
 }
