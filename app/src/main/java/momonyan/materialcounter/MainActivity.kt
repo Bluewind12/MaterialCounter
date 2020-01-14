bcb7a2cbdc3cba918fff902756c4c685d0049f49
“BlueWind_Mac” <zero18030@gmail.com>
2020-01-14 17:32:59 +0900

    [update]Fabのクリック動作の追加。

--- app/src/main/java/momonyan/materialcounter/MainActivity.kt
+++ app/src/main/java/momonyan/materialcounter/MainActivity.kt
@@ -1,12 +1,27 @@
 package momonyan.materialcounter
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
+import momonyan.materialcounter.databinding.ActivityMainBinding
+import momonyan.materialcounter.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // ViewModelのインスタンスを作成
+        val viewModel: MainViewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+
+        // DataBindingのインスタンスを作成（onCreateの外でもよい）
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
+        // ViewModelのインスタンスを作成を設定
+        binding.viewModel = viewModel
+        // ライフサイクル所有者を設定
+        binding.lifecycleOwner = this
     }
 }
---
+++ app/src/main/java/momonyan/materialcounter/viewmodel/MainViewModel.kt
@@ -0,0 +1,11 @@
+package momonyan.materialcounter.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
+class MainViewModel :ViewModel(){
+
+    fun addData(){
+        Log.d("TEST","FABClick")
+    }
+}
