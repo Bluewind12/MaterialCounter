df6d5a6869e1f8e87666b7d85352d429039df2cb
“BlueWind_Mac” <zero18030@gmail.com>
2020-01-16 14:40:07 +0900

    [add]クエリの追加

--- app/src/main/java/momonyan/materialcounter/repository/MaterialDao.kt
+++ app/src/main/java/momonyan/materialcounter/repository/MaterialDao.kt
@@ -1,7 +1,15 @@
 package momonyan.materialcounter.repository
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
 
 @Dao
 interface MaterialDao {
+
+    @Query("SELECT * FROM MaterialEntity")
+    fun getAll()
+
+    @Insert
+    fun addMaterialEntity(vararg materialEntity: MaterialEntity)
 }
